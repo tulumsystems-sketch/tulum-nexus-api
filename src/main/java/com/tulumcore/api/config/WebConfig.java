d46647e7fe0f00b4825f661512239d1cb786a46b
jslabssanjuan-byte <jslabs.sanjuan@gmail.com>
2026-03-22 00:01:33 -0300

    deploy

--- src/main/java/com/tulumcore/api/config/WebConfig.java
+++ src/main/java/com/tulumcore/api/config/WebConfig.java
@@ -15,7 +15,8 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         // Agregá acá el dominio de Netlify cuando lo tengas
                         "https://*.netlify.app",
-                        "https://*.vercel.app"
+                        "https://*.vercel.app",
+                        "https://agent-69bf5a67538bd555905--funny-cuchufli-3b4532.netlify.app"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
